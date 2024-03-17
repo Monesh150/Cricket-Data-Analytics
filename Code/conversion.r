fab5d8dee92bf6656b3754ff4922ef0b63bdd726
gopiprashanthraju <gopiraju738244@gmail.com>
2024-03-17 12:25:36 +0530

    Conversion_BugFixes

--- Code/conversion.r
+++ Code/conversion.r
@@ -20,7 +20,9 @@ Innings <- c(1, 2)
 toss_decision <- c()
 match_winner <- c()
 margin <- c()
+total_score=c()
 for (inn in Innings) {
+  cummulative_score=0
   flag <- TRUE
   balls<-0
   while (balls < 300 && flag) {
@@ -36,17 +38,17 @@ for (inn in Innings) {
       currball <- 1
       extras_count=0
       while (currball <= over_balls) {
-        
         ball_index <- (balls %/% 6) * 6 + currball  # Calculate the current ball index
         toss <- c(toss, jsonData$info$toss$winner)
         match_winner <- c(match_winner, jsonData$info$outcome$winner)
         batter <- jsonData$innings[[inn]]$overs[[over_index]]$deliveries[[currball]]$batter
         bowler <- jsonData$innings[[inn]]$overs[[over_index]]$deliveries[[currball]]$bowler
-        runs <- jsonData$innings[[inn]]$overs[[over_index]]$deliveries[[currball]]$runs$batter
+        runs <- jsonData$innings[[inn]]$overs[[over_index]]$deliveries[[currball]]$runs$batter # nolint: line_length_linter.
         team <- jsonData$innings[[inn]]$team
         batter_vector<-c(batter_vector,batter)
         baller_vector <- c(baller_vector, bowler)
         balls_vector <- c(balls_vector, balls%/%6 + 0.1*(currball-extras_count))
+        cummulative_score=cummulative_score+runs
         runsperball_vector <- c(runsperball_vector, runs)
         country_vector <- c(country_vector, team)
         Innings_vector <- c(Innings_vector, inn)
@@ -55,6 +57,7 @@ for (inn in Innings) {
         if (!is.null(jsonData$innings[[inn]]$overs[[over_index]]$deliveries[[currball]]$extras)) {
           if(is.null(jsonData$innings[[inn]]$overs[[over_index]]$deliveries[[currball]]$extras$legbyes))
           {
+            cummulative_score=cummulative_score+1
             extras_count=extras_count+1
           }
           extra_vector <- c(extra_vector, TRUE)
@@ -66,7 +69,7 @@ for (inn in Innings) {
         } else {
           margin <- c(margin, paste(jsonData$info$outcome$by$wickets, " runs"))
         }
-        
+        total_score=c(total_score,cummulative_score)
         currball <- currball + 1
       }
       
@@ -87,17 +90,21 @@ for (inn in Innings) {
 # print(toss)
 # print(match_winner)
 # Create dataframe from collected data
+print(total_score)
 df <- data.frame(
   baller = baller_vector,
   batter = batter_vector,
   balls = balls_vector,
   runsperball = runsperball_vector,
   extra = extra_vector,
+  total_score=total_score,
   country = country_vector,
   innings = Innings_vector,
   toss_decision = toss_decision,
-  match_winner = match_winner,
-  toss_winner=match_winner
+  toss_winner=match_winner,
+  margin=margin,
+  match_winner = match_winner
 )
 
 write.csv(df,"/Users/morampudigopiprashanthraju/Desktop/DataScience/Minor_Project-II/Data/IND vs AUS/ODI/2003/Conversion_demo.csv")
+print(df)
