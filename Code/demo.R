c39bade07fd785e7674afa8d6f7e3d34cb4659ec
gopiprashanthraju <gopiraju738244@gmail.com>
2024-03-21 23:47:40 +0530

    Updation of Demo File

--- Code/demo.R
+++ Code/demo.R
@@ -4,9 +4,18 @@ library(jsonlite)
 library(hash)
 
 # Read JSON data
-#jsonData <- read_json(path = "/Users/morampudigopiprashanthraju/Desktop/DataScience/Minor_Project-II/Data/IND vs AUS/ODI/2003/65244.json")
+jsonData <- read_json(path = "/Users/morampudigopiprashanthraju/Desktop/DataScience/Minor_Project-II/Data/IND vs AUS/ODI/2003/65244.json")
 #Mushtaq's json path
-jsonData <- read_json(path = "D:/Minor_Project-II/Data/IND vs AUS/ODI/2003/65244.json")
+#jsonData <- read_json(path = "D:/Minor_Project-II/Data/IND vs AUS/ODI/2003/65244.json")
 # Initialize hash
 h <- hash()
 print(jsonData$info$players)
+con=c("India","Australia")
+for(country in con){
+  print(country)
+  for(pla in jsonData$info$players[[country]]){
+    print(pla)
+    h[[pla]]=list(country=country,batting="NONE",bowling_style="None")
+  }
+}
+print(h)
